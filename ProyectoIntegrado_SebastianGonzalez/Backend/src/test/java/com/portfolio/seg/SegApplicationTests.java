7e94f2a08bac5e4c48bc0f22f3850fdc6cb68947
SuciaRata <gsebastianezequiel@gmail.com>
2022-08-08 20:19:00 -0300

    back end

---
+++ ProyectoIntegrado_SebastianGonzalez/Backend/src/test/java/com/portfolio/seg/SegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.seg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
